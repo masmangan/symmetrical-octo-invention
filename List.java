c70a17cf7897d95a2be05da854ab7507a0192870
Marco <marco.mangan@gmail.com>
2026-09-15 16:09:14 +0000

    Listas iniciadas com Deitel e Cormen

---
+++ List.java
@@ -0,0 +1,23 @@
+// Deitel & Deitel
+// 7.13, 7.14,
+// Seções 7.13 e 7.14
+// Capítulo 18
+// Capítulo 19
+// Seções 20.6 e 20.8
+// Capítulo 22
+
+// Cormen et al.
+// Seções 10.2 e 10.3
+
+class Node {
+    int data;
+    Node next;
+}
+
+public class List {
+    public static void main(String[] args) {
+        List l1;
+        l1 = new List();
+        System.out.println(l1);
+    }
+}
